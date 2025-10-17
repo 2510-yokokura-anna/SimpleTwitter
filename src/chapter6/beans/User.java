88894a0b8adb44d6bd1f812ad3ee71265e97e59d
yokokura_anna <yokokura.anna@alhinc.jp>
2025-10-17 17:26:08 +0900

    応用編(簡易Twitter) 　- ログイン機能を実装(途中)

---
+++ src/chapter6/beans/User.java
@@ -0,0 +1,62 @@
+package chapter6.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+
+    private int id;
+    private String account;
+    private String name;
+    private String email;
+    private String password;
+    private String description;
+    private Date createdDate;
+    private Date updatedDate;
+
+    // getter/setterは省略されているので、自分で記述しましょう。
+
+    // getter
+    public String getAccount() {
+        return account;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    // setter
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setAccount(String account) {
+        this.account = account;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+}
---
+++ src/chapter6/controller/TopServlet.java
@@ -0,0 +1,43 @@
+package chapter6.controller;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import chapter6.logging.InitApplication;
+
+
+@WebServlet(urlPatterns = { "/index.jsp" })
+public class TopServlet extends HttpServlet {
+
+
+    /**
+    * ロガーインスタンスの生成
+    */
+    Logger log = Logger.getLogger("twitter");
+
+    /**
+    * デフォルトコンストラクタ
+    * アプリケーションの初期化を実施する。
+    */
+    public TopServlet() {
+        InitApplication application = InitApplication.getInstance();
+        application.init();
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+
+	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
+        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+        request.getRequestDispatcher("/top.jsp").forward(request, response);
+    }
+}
